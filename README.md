a5ab1da27880d251deb0978efe8dce09e07002c3
Matt Healy <healmatt@gmail.com>
2016-03-10 16:28:47 +0800

    Updated API version to 1.1.

--- README.md
+++ README.md
@@ -12,10 +12,8 @@ Authentication Flow
 
 Once you've become an approved integration partner, MyDesktop will supply you with a unique *API Key*. Every request must include a query string parameter `api_key` with your API Key. This is how we identify you as the partner accessing the API.
 
-The MyDesktop user will also need to allow you API access to their MyDesktop database. They will supply you with a unique *Refresh Token*. This token never changes for that user, unless they revoke and re-create the token.
-
-Once you have the *Refresh Token*, you'll need to obtain an *Access Token*. To do so, simply call the `/token` endpoint, using the *Refresh Token* as the HTTP Basic Authentication username. This will generate an *Access Token* which expires in 1 hour. 
+The MyDesktop user will also need to allow you API access to their MyDesktop database. They will supply you with a unique *Access Token*. This token never changes for that user, unless they revoke and re-create the token.
 
 Now, you can use the *Access Token* to access the user's data. For example, you would call the `/contacts` endpoint, using the *Access Token* as the HTTP Basic Authentication username.
 
-When the *Access Token* expires, simply call `/token` to get another one. We recommend building logic in to your application so that it can detect a 401 error and get a new *Access Token* automatically.
+We recommend building logic in to your application to handle a 401 error.
--- php/client.php
+++ php/client.php
@@ -1,41 +1,17 @@
 #!/usr/bin/env php
 <?php
 
-$baseurl = 'https://integrations.mydesktop.com.au/api/v1.0';
+$baseurl = 'https://integrations.mydesktop.com.au/api/v1.1';
 
 $api_key = $argv[1];
-$refresh_token = $argv[2];
+$access_token = $argv[2];
 
-if (!$api_key || !$refresh_token) {
-    echo "Usage: client.php <api_key> <refresh_token>\n";
+if (!$api_key || !$access_token) {
+    echo "Usage: client.php <api_key> <access_token>\n";
     exit();
 }
 
-// Get an Access Token
-$ch = curl_init($baseurl . '/token?api_key=' . $api_key);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-curl_setopt($ch, CURLOPT_USERPWD, $refresh_token . ":" . "");  
-
-$result = curl_exec($ch);
-
-$response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-if ($response_code == 403) {
-    echo "HTTP 403: You don't have access to this operation. Check /scopes for a list of permissions granted to you. Alternatively, check your api_key value.\n";
-    exit();
-} elseif ($response_code == 401) {
-    echo "HTTP 401: Check values for access_token/refresh_token and api_key\n";
-    exit();
-}
-
-$result = json_decode($result);
-
-$access_token = $result->{"token"};
-
-// We now have a token, allowing us to get contacts
-
-// Get an Access Token
+// Get contacts
 $ch = curl_init($baseurl . '/contacts?api_key=' . $api_key);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
@@ -49,7 +25,7 @@ if ($response_code == 403) {
     echo "HTTP 403: You don't have access to this operation. Check /scopes for a list of permissions granted to you. Alternatively, check your api_key value.\n";
     exit();
 } elseif ($response_code == 401) {
-    echo "HTTP 401: Check values for access_token/refresh_token and api_key\n";
+    echo "HTTP 401: Check values for access_token and api_key\n";
     exit();
 }
 
